ed24eaf3d0b46971fbdf28e572553396220e2379
Proksimas <valentinpajaud@gmail.com>
2025-03-20 17:08:10 +0100

    add learning data items

--- hackeridle/Game/Clickers/learning_clicker.gd
+++ hackeridle/Game/Clickers/learning_clicker.gd
@@ -1,5 +1,4 @@
-extends Node2D
-
+extends StaticBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +7,22 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
+
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			Player.knowledge_point += get_earn_kp()
+
+
+
+func get_earn_kp() -> float:
+	var gain: float
+	for learning_item_name in Player.learning_item_bought:
+		var level_item = Player.learning_item_bought[learning_item_name]["level"]
+		var knowledge_point_earned = Player.learning_item_bought[learning_item_name]["knowledge_point_earned"]
+		gain += level_item * knowledge_point_earned
+	
+	return gain
 	pass
--- hackeridle/Game/Singleton/global.gd
+++ hackeridle/Game/Singleton/global.gd
@@ -1,18 +1,20 @@
 extends Node
 
-var knowledge_point: float:
-	set(value):
-		return clamp(value, 0, INF)
-		
-var hacking_point: float:
-	set(value):
-		return clamp(value, 0, INF)
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
+func load_json(path: String) -> Dictionary:
+	if not FileAccess.file_exists(path):
+		print("Erreur: Le fichier JSON n'existe pas.")
+		return {}
+
+	var file = FileAccess.open(path, FileAccess.READ)
+	var content = file.get_as_text()
+	file.close()
+
+	var json_result = JSON.parse_string(content)
+	if json_result is Dictionary:
+		return json_result
+	else:
+		print("Erreur: Le fichier JSON n'est pas valide.")
+		return {}
