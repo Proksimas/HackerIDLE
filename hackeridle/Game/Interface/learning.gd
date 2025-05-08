de004b113dc9166a049b985bf6e713c912a9661c
Proksimas <valentinpajaud@gmail.com>
2025-05-08 19:53:37 +0200

    brain begin bigger

--- hackeridle/Game/Interface/learning.gd
+++ hackeridle/Game/Interface/learning.gd
@@ -2,11 +2,19 @@ extends Control
 
 
 @onready var passif_clickers: HFlowContainer = %PassifClickers
-@onready var clicker_arc: AspectRatioContainer = $VBoxContainer/CenterContainer/ClickerARC
+@onready var clicker_arc: AspectRatioContainer = %ClickerARC
+
 
 const LEARNING_CLICKER = preload("res://Game/Clickers/learning_clicker.tscn")
 const CLICK_PARTICLES = preload("res://Game/Graphics/ParticlesAndShaders/click_particles.tscn")
 
+var clicker_scale = Vector2(10,10)
+var button_cliked: bool = false
+var clicker_arc_original_size
+
+func _ready() -> void:
+	clicker_arc_original_size = clicker_arc.custom_minimum_size
+	
 func set_learning_clicker():
 	return # obsolète donc return
 	_clear()
@@ -35,4 +43,19 @@ func _on_clicker_button_pressed() -> void:
 	Player.brain_level += 1
 	Player.knowledge_point += 1 # A CHANGER
 	
-	pass # Replace with function body.
+	button_cliked = true
+	clicker_arc.custom_minimum_size = clicker_arc.custom_minimum_size + clicker_scale
+	
+	#var tween = get_tree().create_tween()
+	#tween.tween_property(clicker_arc, "custom_minimum_size", 
+						#clicker_arc.custom_minimum_size + clicker_scale, 1)
+	##tween.set_parallel(true)
+	
+	
+func _process(delta: float) -> void:
+	if button_cliked:
+		var tween = get_tree().create_tween()
+		tween.tween_property(clicker_arc, "custom_minimum_size", 
+						clicker_arc_original_size, 1).from(clicker_arc.custom_minimum_size)
+		button_cliked = false
+		
