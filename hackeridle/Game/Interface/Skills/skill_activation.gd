22d14b89a7625e31346a8cd5e4f2c429f3c32283
Proksimas <valentinpajaud@gmail.com>
2025-06-11 14:20:42 +0200

    add_ski

--- hackeridle/Game/Interface/Skills/skill_activation.gd
+++ hackeridle/Game/Interface/Skills/skill_activation.gd
@@ -8,6 +8,13 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 func set_skill_activation(skill:ActiveSkill):
+	self.show()
 	skill_associated = skill
 	skill_button.texture_normal = skill.as_texture
 	pass
+
+
+
+func _on_skill_button_pressed() -> void:
+	skill_associated.launch_as()
+	pass # Replace with function body.
