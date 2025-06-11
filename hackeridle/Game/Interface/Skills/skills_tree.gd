6296f937863379ba22de43345ff803d8998c9fce
Proksimas <valentinpajaud@gmail.com>
2025-06-11 19:10:11 +0200

    skills works without cost

--- hackeridle/Game/Interface/Skills/skills_tree.gd
+++ hackeridle/Game/Interface/Skills/skills_tree.gd
@@ -2,15 +2,14 @@ extends Control
 @onready var skill_name_label: Label = %SkillNameLabel
 @onready var skill_desc_label: Label = %SkillDescLabel
 @onready var buy_skill_button: Button = %BuySkillButton
+@onready var skills_grid: GridContainer = %SkillsGrid
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	for skill in skills_grid.get_children():
+		skill.skill_button_pressed.connect(_on_skill_node_skill_button_pressed)
 	pass # Replace with function body.
-
-
-
-
 
 
 func _on_skill_node_skill_button_pressed(skill_name: String) -> void:
