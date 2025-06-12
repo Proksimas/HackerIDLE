2fab8f2c4192f48e85c2c6c57751e1c06d7e4478
Proksimas <valentinpajaud@gmail.com>
2025-06-12 18:01:53 +0200

    skill progress but have tmp issue

--- hackeridle/Game/Interface/Skills/skills_tree.gd
+++ hackeridle/Game/Interface/Skills/skills_tree.gd
@@ -3,6 +3,7 @@ extends Control
 @onready var skill_desc_label: Label = %SkillDescLabel
 @onready var buy_skill_button: Button = %BuySkillButton
 @onready var skills_grid: GridContainer = %SkillsGrid
+@onready var cost_sp_label: Label = %CostSPLabel
 
 
 # Called when the node enters the scene tree for the first time.
@@ -13,5 +14,10 @@ func _ready() -> void:
 
 
 func _on_skill_node_skill_button_pressed(skill_name: String) -> void:
+	
+	var skills_cara = SkillsManager.get_skill_cara(skill_name) 
+	skill_name_label.text = tr(skills_cara['as_name'])
+	skill_desc_label.text = tr(skills_cara['as_name'] + "_desc")
+	cost_sp_label.text = str(skills_cara['cost'][skills_cara["as_level"]])
 	SkillsManager.learn_as(skill_name)
 	pass # Replace with function body.
