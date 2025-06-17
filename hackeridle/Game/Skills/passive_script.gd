e4634c3e870d4f4a93f6565a2957f70cdbd642f3
Proksimas <valentinpajaud@gmail.com>
2025-06-17 11:11:53 +0200

    passiveskill in progress

---
+++ hackeridle/Game/Skills/passive_script.gd
@@ -0,0 +1,28 @@
+extends Resource
+class_name PassiveSkill
+
+@export var cost:Array   #son cout en skill point = le niveau
+@export var as_texture: Texture
+@export var as_name: String
+
+var ps_level = 0
+var tree
+
+	
+func attach(_caster: Node, level) -> void:
+	""" OBLIGATOIRE lors de l'instantiation d'un skill
+	Appeler lorsqu'on add le skill, cela permet de gérer ce que fait le skill:
+		- les connexions à appeler
+		- l'ajout des stats brut
+		
+		A SURCHARGER """
+		
+	tree = _caster.get_tree()   # on récupère la référence de l'arbre
+	self.ps_level = level
+	
+func detach(_caster: Node)-> void:
+	"""dettache les ajouts que donne le sill
+	
+	A SURCHARGER """
+	pass
+	
--- hackeridle/Game/Skills/skills_manager.gd
+++ hackeridle/Game/Skills/skills_manager.gd
@@ -9,16 +9,27 @@ var active_skills: Dictionary = {
 var passives_skills: Dictionary = {}
 
 signal as_learned(skill:ActiveSkill)
+signal ps_learned(skill:PassiveSkill)
 
+func learn_ps(skill_name: String):
+	if !passives_skills.has(skill_name):
+		push_warning("Le skill %s à ajouter n'existe pas" % [skill_name])
+	var skill:PassiveSkill = passives_skills[skill_name].duplicate()
+	for resource:PassiveSkill in Player.skills_owned['passive']:
+		if resource.ps_name == skill_name:
+			#TODO
+			push_error("Le joueur a deja le skill %s" % [skill_name])
+			return
+
+	Player.skills_owned["passive"].append(skill)
+	skill.attach(Player, 1) #on met au niveau 1
+	ps_learned.emit(skill)
 
 
 func learn_as(skill_name: String):
 	if !active_skills.has(skill_name):
 		push_warning("Le skill %s à ajouter n'existe pas" % [skill_name])
-	
 	var skill:ActiveSkill = active_skills[skill_name].duplicate()
-	
-	
 	for resource:ActiveSkill in Player.skills_owned['active']:
 		if resource.as_name == skill_name:
 			#TODO
@@ -47,7 +58,6 @@ func get_skill_cara(skill_name: String):
 		var usage = prop.usage
 		if usage & PROPERTY_USAGE_SCRIPT_VARIABLE:
 			list[p_name] = skills[skill_name].get(prop["name"])
-	
 	return list
 
 	
