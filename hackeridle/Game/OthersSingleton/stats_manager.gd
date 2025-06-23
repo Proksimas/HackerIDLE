70b234ec9048a93688c769b52745cf01f60077d5
Proksimas <valentinpajaud@gmail.com>
2025-06-23 19:08:39 +0200

    ok rework begins

--- hackeridle/Game/OthersSingleton/stats_manager.gd
+++ hackeridle/Game/OthersSingleton/stats_manager.gd
@@ -1,7 +1,8 @@
 extends Node
 
 enum Stats{GOLD, KNOWLEDGE, BRAIN_XP}
-enum MODIFIER_TYPE {PERCENTAGE, FLAT}
+enum ModifierType {PERCENTAGE, FLAT}
+enum TargetModifier {GLOBAL, BRAIN_CLICK}
 
 const STATS_NAMES = {
 	Stats.GOLD: "gold",
@@ -9,20 +10,28 @@ const STATS_NAMES = {
 	Stats.BRAIN_XP: "brain_xp"
 }
 
-var modifiers : Dictionary = {}    #tous les modificateurs des stats
+var modifiers: Dictionary = {}    #tous les modificateurs des stats
+var brain_click_modifiers: Dictionary = {}
 
 
 func _ready() -> void:
 	_init()
 	
-	add_modifier(Stats.GOLD, MODIFIER_TYPE.PERCENTAGE, 0.10, "test")
-
+	add_modifier(TargetModifier.BRAIN_CLICK, Stats.GOLD, ModifierType.PERCENTAGE, 0.10, "test")
+	print(modifiers)
+	print(brain_click_modifiers)
+	
 func _init() -> void:
 	"""on doit mettre les stats à calculer"""
+	modifiers = {}    #tous les modificateurs des stats
+	brain_click_modifiers = {}
 	for stat in Stats.values():
 		modifiers[stat] = []
+		brain_click_modifiers[stat] = []
 
-func add_modifier(stat_name: Stats, modifier_type: MODIFIER_TYPE, value: float, source: String = ""):
+
+func add_modifier(target_modifier:TargetModifier, stat_name: Stats, \
+			modifier_type: ModifierType, value: float, source: String = ""):
 
 	if !modifiers.has(stat_name):
 		push_error("la stat %s n'existe pas pour les modification" % stat_name)
@@ -33,11 +42,16 @@ func add_modifier(stat_name: Stats, modifier_type: MODIFIER_TYPE, value: float, 
 		"value": value,
 		"source": source
 	} 
-	modifiers[stat_name].append(new_modifier)
+	match target_modifier:
+		TargetModifier.GLOBAL:
+			modifiers[stat_name].append(new_modifier)
+		TargetModifier.BRAIN_CLICK:
+			brain_click_modifiers[stat_name].append(new_modifier)
 	
 	
 	
-func remove_modifier(stat_name: String, modifier_to_remove:Dictionary):
+func remove_modifier(target_modifier:TargetModifier, stat_name: String,\
+					 modifier_to_remove:Dictionary):
 	if modifiers.has(stat_name):
 		var index =modifiers[stat_name].find(modifier_to_remove)
 		if index != -1:
