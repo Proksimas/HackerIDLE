c4a471efa775d402bf2e78d48102609f8c58db55
Proksimas <valentinpajaud@gmail.com>
2025-08-05 19:35:26 +0200

    l'ia m'a fixe le probleme...

--- hackeridle/Game/News/news_panel.gd
+++ hackeridle/Game/News/news_panel.gd
@@ -13,10 +13,10 @@ extends PanelContainer
 @onready var news_history: Panel = %NewsHistory
 @onready var news_paper_icon: TextureButton = %NewsPaperIcon
 
-@export var scroll_speed_pixels_per_second: float = 100.0 
+@export var scroll_speed_pixels_per_second: float = 100.0
 @export var scrolling_time: int = 2
 
-enum NewsType {BREAKING, CHRONOLOGICAL, RANDOM, BANDEAU}
+enum NewsType {BREAKING, CHRONOLOGICAL, RANDOM}
 
 const GENERIC = "res://Game/News/TextFiles/generic.csv"
 const BULLET_POINT = preload("res://Game/Interface/Specials/bullet_point.tscn")
@@ -27,12 +27,12 @@ var scroll_starting: bool = false
 var news_size
 var news_cache: Array = [] # [ {news_type}: news_key
 
-var breaking_news_passed: Array = []  #[dates]
+var breaking_news_passed: Array = [] #[dates]
 
 var chronological_news_passed: Array = []
 
-var nb_of_msg = {"introduction": 2,   # key_de_la_traduction : nb of message associés
-				"random": 1
+var nb_of_msg = {"introduction": 2, # key_de_la_traduction : nb of message associés
+				 "random": 1
 }
 
 signal news_finished
@@ -40,132 +40,111 @@ signal news_finished
 func _ready() -> void:
 	news_history_label.clear()
 	news_size = text_label_container.size.x
-	#display_news(pick_random_sentence("introduction"), NewsType.RANDOM)
 	new_news()
 	
 	StatsManager.s_add_infamy.connect(_on_s_add_infamy)
 	StatsManager.s_infamy_effect_added.connect(draw_infamy_stats)
-	TimeManager.s_date.connect(_on_s_date)  
+	TimeManager.s_date.connect(_on_s_date)
 	_on_s_add_infamy(StatsManager.infamy["current_value"])
 	infamy_stats.hide()
 	news_history.hide()
-	pass # Replace with function body.
+	pass
 
 
 func _process(_delta: float) -> void:
 	if scroll_starting:
-		# Calculer le déplacement basé sur le temps écoulé (_delta)
 		var move_amount = scroll_speed_pixels_per_second * _delta
-		# Déplacer le texte vers la gauche
 		news_container.position.x -= move_amount
 
 		if news_container.position.x <= 0 - news_container.get_minimum_size().x:
 			scroll_starting = false
 			news_finished.emit()
 			news_container.position.x = get_viewport_rect().size.x
+
+func swap_panel_to_bandeau(is_breaking_news: bool):
+	var new_color = Color(0, 0, 0)
+	if is_breaking_news:
+		new_color = Color(255, 0, 0)
 	
-func swap_panel_to_bandeau():
-	var new_color = Color(255,0,0)
 	var stylebox = StyleBoxFlat.new()
 	stylebox.bg_color = new_color
-	news_color_rect.remove_theme_stylebox_override("panel")
 	news_color_rect.add_theme_stylebox_override("panel", stylebox)
-	text_label_container.hide()
-	breaking_news_container.show()
 	
-func display_news(news_key: String, type: NewsType):
-	if type == NewsType.BREAKING: 
-		swap_panel_to_bandeau()
-		self.news_finished.connect(_on_news_finished.bind(news_key, NewsType.BANDEAU))
-	elif type == NewsType.BANDEAU:
-		swap_panel_to_bandeau()
-		self.news_finished.connect(_on_news_finished.bind(news_key, NewsType.BREAKING))
+	if is_breaking_news:
+		text_label_container.hide()
+		breaking_news_container.show()
 	else:
-		var new_color = Color(0,0,0)
-		var stylebox = StyleBoxFlat.new()
-		stylebox.bg_color = new_color
-		news_color_rect.remove_theme_stylebox_override("panel")
-		news_color_rect.add_theme_stylebox_override("panel", stylebox)
 		text_label_container.show()
 		breaking_news_container.hide()
-		self.news_finished.connect(_on_news_finished.bind(news_key, type))
-		
+
+func display_news(news_key: String, type: NewsType):
+	# D'abord, on s'assure que le signal est déconnecté avant d'en connecter un nouveau
+	if news_finished.is_connected(_on_news_finished):
+		news_finished.disconnect(_on_news_finished)
+	
+	swap_panel_to_bandeau(type == NewsType.BREAKING)
+	
+	self.news_finished.connect(_on_news_finished)
+	
 	text_label.text = tr(news_key)
 	news_container.position.x = get_viewport_rect().size.x
 	news_container.position = Vector2(news_size, news_container.position.y)
 	scroll_starting = true
 
-		
-	
 func pick_random_sentence(key: String):
-	if !nb_of_msg.has(key):
+	if not nb_of_msg.has(key):
 		push_error("La clé de traduction n'est pas valide.")
-	var random =randi_range(1, nb_of_msg[key])
+	var random = randi_range(1, nb_of_msg[key])
 	
 	return (key + "_" + str(random))
-	
-func _on_news_finished(_news_key:String, _news_type: NewsType):
-	self.news_finished.disconnect(_on_news_finished)
-	refresh_news_history()
-	# ATTENTION Quand on reçoit le type BANDEAU, cad que le bandeau est terminé
-	# On doit donc display la breaking news, en envoyant BANDEAU
-	if _news_type == NewsType.BANDEAU:
-		#on display la news
-		display_news(_news_key, NewsType.BANDEAU)
-	else:
-		new_news()
-	pass
-	
 
+func _on_news_finished():
+	if news_finished.is_connected(_on_news_finished):
+		news_finished.disconnect(_on_news_finished)
+	refresh_news_history()
+	new_news()
 
 func _on_s_date(date):
 	var formatted_date_1: String = TimeManager.get_formatted_date_string(date)
 	var breaking_news_has_trad = tr(formatted_date_1)
 	var chronogical_news_has_trad = tr("$" + formatted_date_1)
 	
-	if breaking_news_has_trad != formatted_date_1: # il y a une traduction d'une breaking news
-		#breaking_news_cache.append("breaking_news_template")
-		
+	if breaking_news_has_trad != formatted_date_1:
 		news_cache.append({NewsType.BREAKING: formatted_date_1})
-		
+	
 	elif chronogical_news_has_trad != "$" + formatted_date_1:
 		news_cache.append({NewsType.CHRONOLOGICAL: "$" + formatted_date_1})
-		
-
 
 func new_news():
-	if !news_cache.is_empty():
-		#	TODO mais traité en priorité
-		var next_news:Dictionary = news_cache.pop_front()
-		match next_news.keys()[0]:
-			NewsType.BREAKING:
-				display_news(next_news.values()[0], NewsType.BREAKING)
-				breaking_news_passed.append(next_news.values()[0])
-			NewsType.CHRONOLOGICAL:
-				display_news(next_news.values()[0], NewsType.CHRONOLOGICAL)
-				chronological_news_passed.append(next_news.values()[0])
+	if not news_cache.is_empty():
+		var next_news: Dictionary = news_cache.pop_front()
+		var news_key = next_news.values()[0]
+		var news_type = next_news.keys()[0]
 		
+		display_news(news_key, news_type)
+		
+		if news_type == NewsType.BREAKING:
+			breaking_news_passed.append(news_key)
+		elif news_type == NewsType.CHRONOLOGICAL:
+			chronological_news_passed.append(news_key)
 		return
-	#toutes les news importantes sont passées. On lance donc une news random
+	
 	display_news(pick_random_sentence("random"), NewsType.RANDOM)
-
-			
-var news_to_show:int = 0 
+	
+var news_to_show:int = 0
 func refresh_news_history():
 	news_history_label.text = ""
 	match news_to_show:
 		2:
 			for elmt in breaking_news_passed:
-				news_history_label.text += " [color=yellow]%s[/color]    %s\n" % [elmt, tr(elmt)]
+				news_history_label.text += " [color=yellow]%s[/color]   %s\n" % [elmt, tr(elmt)]
 		1:
 			for elmt2 in chronological_news_passed:
-				news_history_label.text += " [color=yellow]%s[/color]    %s\n" % [elmt2.trim_prefix("$"), tr(elmt2)]
-
-
+				news_history_label.text += " [color=yellow]%s[/color]   %s\n" % [elmt2.trim_prefix("$"), tr(elmt2)]
 
 func _on_news_paper_icon_pressed() -> void:
 	match news_to_show:
-		0: #on affiche les news classiques, e ton prépare le bouton du beaking news
+		0:
 			news_history.visible = true
 			news_paper_icon.texture_normal = BREAKING_NEWS_ICON
 			news_to_show = 1
@@ -177,35 +156,30 @@ func _on_news_paper_icon_pressed() -> void:
 			news_paper_icon.flip_h = false
 			news_history.visible = false
 			news_to_show = 0
-	
+			
 	refresh_news_history()
-
+	
 #region INFAMY
 
 
-## On gère l'infamie
-
-	
 func _on_s_add_infamy(_infamy_value):
 	infamy_value.text = str(_infamy_value)
-	
 
 func _on_infamy_icon_pressed() -> void:
-	infamy_stats.visible = !infamy_stats.visible
-	pass # Replace with function body.
+	infamy_stats.visible = not infamy_stats.visible
 
 func draw_infamy_stats():
-	"""Dessine les caractéristiques liées à l'infamie actuelle"""
 	for effect in infamy_effects.get_children():
 		effect.queue_free()
-		
+	
 	treshold_name_label.text = tr("$" + StatsManager.INFAMY_NAMES.get(StatsManager.get_infamy_treshold()))
 	var _hack_modifiers = StatsManager.hack_modifiers
-	var _translations:Array = []
+	var _translations: Array = []
+	
 	for stat: StatsManager.Stats in _hack_modifiers:
 		if _hack_modifiers[stat].is_empty():
 			continue
-			
+		
 		var hack_dicts = _hack_modifiers[stat]
 		var value: float
 		var has_value: bool = false
@@ -213,9 +187,9 @@ func draw_infamy_stats():
 			if dict["source"].begins_with("infamy_"):
 				value = dict["value"] * 100
 				has_value = true
-				
-		if has_value == false:
-			push_warning("Pas valeur trouvée, pas normal ")
+		
+		if not has_value:
+			push_warning("Pas de valeur trouvée, pas normal ")
 			return
 		
 		var value_str: String
@@ -225,15 +199,13 @@ func draw_infamy_stats():
 			value_str = "-%s" % str(abs(value))
 		else:
 			value_str = ""
-		_translations.append(tr("hack_" + StatsManager.STATS_NAMES.get(stat)).\
-				format({"hack_" + StatsManager.STATS_NAMES.get(stat) + "_value": value_str}))
+		
+		_translations.append(tr("hack_" + StatsManager.STATS_NAMES.get(stat)).format({"hack_" + StatsManager.STATS_NAMES.get(stat) + "_value": value_str}))
 	
 	for trad in _translations:
 		var bullet_label = BULLET_POINT.instantiate()
 		infamy_effects.add_child(bullet_label)
 		bullet_label.set_bullet_point(trad)
-
-	#
 
 func _draw():
 	infamy_value.text = str(StatsManager.infamy["current_value"])
@@ -241,10 +213,7 @@ func _draw():
 
 func _on_cheat_infamy_pressed() -> void:
 	StatsManager.add_infamy(5)
-	pass # Replace with function body.
-
 
 func _on_cheat_infamy_2_pressed() -> void:
 	StatsManager.add_infamy(-5)
-	pass # Replace with function body.
 #endregion
