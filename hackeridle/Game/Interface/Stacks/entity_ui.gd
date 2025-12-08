4a2682301e4744a6b3d38bd47e56adc5947f5407
Proksimas <valentinpajaud@gmail.com>
2025-12-08 19:46:27 +0100

    stack ui in progress

---
+++ hackeridle/Game/Interface/Stacks/entity_ui.gd
@@ -0,0 +1,14 @@
+extends Control
+@onready var stack_name_label: Label = %StackNameLabel
+@onready var entity_name_label: Label = %EntityNameLabel
+@onready var stack_grid: GridContainer = %StackGrid
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
