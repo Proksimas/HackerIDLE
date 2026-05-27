1bc920c0c52fe706ade2794b227ccc4707d537ae
Proksimas <valentinpajaud@gmail.com>
2026-05-27 20:04:20 +0200

    impropve ui of novanet

--- hackeridle/Game/Interface/NovaNet/nova_net_tab.gd
+++ hackeridle/Game/Interface/NovaNet/nova_net_tab.gd
@@ -1,18 +1,28 @@
 extends TabContainer
 
-const TAB_TITLE_KEYS := ["NovaNet", "$Bots", "$cyber_force", "Stacks"]
+const TAB_INVENTORY := 0
+const TAB_BOTS := 1
+const TAB_CYBER_FORCE := 2
+const TAB_FIGHT := 3
 
-@onready var tab_buttons: Array[VBoxContainer] = [
-	%NovaNetTabButton0,
-	%NovaNetTabButton1,
-	%NovaNetTabButton2,
-	%NovaNetTabButton3,
+const TAB_TITLE_KEYS := [
+	"$inventory",
+	"$bot",
+	"$cyber_force",
+	"$fight",
+]
+
+@onready var tab_buttons: Array[Button] = [
+	%InventoryTabButton,
+	%BotsTabButton,
+	%CyberForceTabButton,
+	%FightTabButton,
 ]
 @onready var tab_title_labels: Array[Label] = [
-	%NovaNetTabTitle0,
-	%NovaNetTabTitle1,
-	%NovaNetTabTitle2,
-	%NovaNetTabTitle3,
+	%InventoryTabTitle,
+	%BotsTabTitle,
+	%CyberForceTabTitle,
+	%FightTabTitle,
 ]
 
 
@@ -23,7 +33,6 @@ func _ready() -> void:
 func set_buttons() -> void:
 	tabs_visible = false
 	_clear_native_tabs()
-	_ensure_tab_button_connections()
 	refresh_buttons()
 	_update_tab_buttons_state()
 	if not tab_changed.is_connected(_on_tab_changed):
@@ -32,16 +41,7 @@ func set_buttons() -> void:
 
 func refresh_buttons() -> void:
 	for i in range(min(tab_title_labels.size(), TAB_TITLE_KEYS.size())):
-		match i:
-			0:
-				tab_title_labels[i].text = tr("$inventory")
-			1:
-				tab_title_labels[i].text = tr("$bot")
-			2:
-				tab_title_labels[i].text = tr("$cyber_force")
-			3:
-				tab_title_labels[i].text = tr("$fight")
-
+		tab_title_labels[i].text = tr(TAB_TITLE_KEYS[i])
 
 
 func _clear_native_tabs() -> void:
@@ -50,38 +50,25 @@ func _clear_native_tabs() -> void:
 		set_tab_icon(i, null)
 
 
-func _ensure_tab_button_connections() -> void:
-	var callbacks := [
-		_on_nova_net_tab_button_0_gui_input,
-		_on_nova_net_tab_button_1_gui_input,
-		_on_nova_net_tab_button_2_gui_input,
-		_on_nova_net_tab_button_3_gui_input,
-	]
-	for i in range(min(tab_buttons.size(), callbacks.size())):
-		if not tab_buttons[i].gui_input.is_connected(callbacks[i]):
-			tab_buttons[i].gui_input.connect(callbacks[i])
+func _select_tab(tab_index: int) -> void:
+	current_tab = tab_index
+	_update_tab_buttons_state()
 
 
-func _on_nova_net_tab_button_0_gui_input(event: InputEvent) -> void:
-	_on_tab_button_gui_input(event, 0)
+func _on_inventory_tab_button_pressed() -> void:
+	_select_tab(TAB_INVENTORY)
 
 
-func _on_nova_net_tab_button_1_gui_input(event: InputEvent) -> void:
-	_on_tab_button_gui_input(event, 1)
+func _on_bots_tab_button_pressed() -> void:
+	_select_tab(TAB_BOTS)
 
 
-func _on_nova_net_tab_button_2_gui_input(event: InputEvent) -> void:
-	_on_tab_button_gui_input(event, 2)
+func _on_cyber_force_tab_button_pressed() -> void:
+	_select_tab(TAB_CYBER_FORCE)
 
 
-func _on_nova_net_tab_button_3_gui_input(event: InputEvent) -> void:
-	_on_tab_button_gui_input(event, 3)
-
-
-func _on_tab_button_gui_input(event: InputEvent, tab_index: int) -> void:
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		current_tab = tab_index
-		_update_tab_buttons_state()
+func _on_fight_tab_button_pressed() -> void:
+	_select_tab(TAB_FIGHT)
 
 
 func _on_tab_changed(_tab_index: int) -> void:
